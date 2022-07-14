a03d5ddcbf7e628bc6713533d7fd1e0805a6a229
AnnemariaRe <72071649+AnnemariaRe@users.noreply.github.com>
2022-07-14 19:55:43 +0300

    Delete kotiki-java/dao/src/main directory

--- kotiki-java/dao/src/main/java/org/annemariare/kotiki/exception/EntityAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package org.annemariare.kotiki.exception;
-
-public class EntityAlreadyExistsException extends RuntimeException {
-    public EntityAlreadyExistsException() {
-        super("Entity already exists :)");
-    }
-}
